b04a4f934e7a9064310057d0c5fb1c9a2849a95a
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2023-08-13 19:09:41 +0100

    Create 3Sensor_v1.ino

---
+++ Esp32 Software/Esp32_3Sensor/3Sensor_v1/3Sensor_v1.ino
@@ -0,0 +1,40 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Pin for the 1-Wire bus
+const int oneWireBusPin = 5;  // You can choose any available GPIO pin
+
+// Create a OneWire instance to communicate with the sensors
+OneWire oneWire(oneWireBusPin);
+
+// Pass the OneWire instance to the DallasTemperature library
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(115200);
+  sensors.begin();  // Initialize the library
+}
+
+void loop() {
+  sensors.requestTemperatures();  // Send command to all sensors to get temperatures
+  
+  // Read temperatures from each sensor
+  float temp1 = sensors.getTempCByIndex(0);
+  float temp2 = sensors.getTempCByIndex(1);
+  float temp3 = sensors.getTempCByIndex(2);
+  
+  Serial.print("Sensor 1: ");
+  Serial.print(temp1);
+  Serial.println(" °C");
+  
+  Serial.print("Sensor 2: ");
+  Serial.print(temp2);
+  Serial.println(" °C");
+  
+  Serial.print("Sensor 3: ");
+  Serial.print(temp3);
+  Serial.println(" °C");
+  
+  delay(1000);  // Wait for a second before reading again
+}
+
