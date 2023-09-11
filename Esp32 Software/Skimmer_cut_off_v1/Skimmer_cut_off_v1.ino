1994eebbdacac3e985edc16373085b3a2f161fbd
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2023-09-11 20:06:03 +0100

    Update Skimmer_cut_off_v1.ino

--- Esp32 Software/Skimmer_cut_off_v1/Skimmer_cut_off_v1.ino
+++ Esp32 Software/Skimmer_cut_off_v1/Skimmer_cut_off_v1.ino
@@ -5,9 +5,11 @@
 #define MQTT_PORT 1883
 
 const int skimmerSensor = 13;
-const int pumpCutoff = 5;
+const int pumpCutoff = 14;
+const int ledPin = 2;
 
 const char* MQTT_PUB_CUTOFF = "SkimmerState";
+const char* SKIMMER_HEART_TOPIC = "SkimmerHeartState";
 
 String currentSkimmerState = "";  // To store skimmer state
 
@@ -28,6 +30,7 @@ void connectToMqtt() {
     Serial.println("Attempting MQTT connection...");
     if (mqttClient.connect("skimmerCutOff")) {
       Serial.println("Connected to MQTT broker");
+      mqttClient.subscribe(SKIMMER_HEART_TOPIC);
     } else {
       Serial.print("Failed, rc=");
       Serial.print(mqttClient.state());
@@ -49,26 +52,56 @@ void readAndDetermineStates() {
   }
 }
 
+void turnOnLedFor500ms() {
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  // Convert payload to a string
+  String message = String((char*)payload, length);
+
+  Serial.print("Received message on topic: ");
+  Serial.println(topic);
+  Serial.print("Message: ");
+  Serial.println(message);
+  
+  
+  if (strcmp(topic, SKIMMER_HEART_TOPIC) == 0) {
+    if (message == "on") {
+      turnOnLedFor500ms();
+    }
+  }
+}
+
+
 void controlPump() {
   if (currentSkimmerState == "skimmer high"){
-    digitalWrite(pumpCutoff, LOW);
+    digitalWrite(pumpCutoff, HIGH);
+    //digitalWrite(ledPin, HIGH);
     
   }
   if (currentSkimmerState == "skimmer normal") {
-    digitalWrite(pumpCutoff, HIGH);
+    digitalWrite(pumpCutoff, LOW);
+    //digitalWrite(ledPin, LOW);
   }
 }
 
 void setup() {
   Serial.begin(115200);
   pinMode(pumpCutoff, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   pinMode(skimmerSensor, INPUT_PULLUP);
 
   connectToWifi();
+
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
   mqttClient.setKeepAlive(120);
+  mqttClient.setCallback(callback);
 
-  digitalWrite(pumpCutoff, HIGH);
+  digitalWrite(pumpCutoff, LOW);
+  digitalWrite(ledPin, LOW);
   delay(5000);
 }
 
@@ -79,7 +112,7 @@ void loop() {
   if (!mqttClient.connected()) {
     connectToMqtt();
   }
-  
+  mqttClient.loop();
   // Call the function to read sensor values and determine states
   readAndDetermineStates();
 
